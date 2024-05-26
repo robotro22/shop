36ac5a70bddbeb9423487cad4cdffd0bb559ede7
Nurul <erkinkazhy.n@gmail.com>
2024-05-26 16:44:17 +0600

    e

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\admin\\PycharmProjects\\pythonProject10\\test.db'
+app.config['SECRET_KEY']='123'
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class ProductForm(FlaskForm):
+    name = StringField('name of your product')
+    description = StringField('description')
+    price = StringField('price')
+    submit = SubmitField('add your product')
 
 @app.route('/')
 def home():
@@ -8,11 +28,25 @@ def home():
 
 @app.route('/shop')
 def shop():
-    return render_template('shop.html')
+    products = Product.query.all()
+    return render_template('shop.html', products=products)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_products():
+    form = ProductForm()
+    if form.validate_on_submit():
+        new_product = Product(name = form.name.data, description=form.description.data, price=form.price.data)
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('shop'))
+    return render_template('add.html',form=form)
+
 
 @app.route('/contacts')
 def contacts():
     return render_template('contact.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
